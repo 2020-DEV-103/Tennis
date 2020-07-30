38453b7b922416eab42c083ed6e55fa95c1ad3cc
2020-DEV-103 <2020dev103@gmail.com>
2020-07-30 07:05:49 +0530

    feat: the game score should be zero for first player before game begins

---
+++ src/test/java/com/bnpp/kata/tennis/TennisGameTest.java
@@ -0,0 +1,16 @@
+package com.bnpp.kata.tennis;
+
+import static org.junit.Assert.assertThat;
+
+import org.hamcrest.CoreMatchers;
+import org.junit.Test;
+
+public class TennisGameTest {
+
+	@Test
+	public void firstPlayerScoredShouldBeZeroBeforeGameBegins() {
+		TennisGame tennisGame = new TennisGame();
+
+		assertThat(0, CoreMatchers.is(tennisGame.getFirstPlayerScoredPoint()));
+	}
+}
